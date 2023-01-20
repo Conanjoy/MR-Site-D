2bd381677187d4e40091f54c571f1990c598a474
Conanjoy <119912899+Conanjoy@users.noreply.github.com>
2023-01-20 08:48:24 +0530

    Update Files

--- ms_rewards_farmer.py
+++ ms_rewards_farmer.py
@@ -20,6 +20,7 @@ from pyvirtualdisplay import Display
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
